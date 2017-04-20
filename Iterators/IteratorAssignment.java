415e9927ae798fc0962cef9f0d6ffc82207c1cfb
Kyle <klin12@stuy.edu>
2017-04-20 13:57:43 -0400

    Added ./Iterators/IteratorAssignment.java and wrote barebones code for it

---
+++ Iterators/IteratorAssignment.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
+
+public class IteratorAssignment {
+
+	// For-each loop
+	public static int countEvensFE(List<Integer> L) {
+		
+	}
+	
+	// Using iterator directly
+	public static int countEvensI(List<Integer> L) {
+	
+	}
+	
+	// Using the get(int index) method
+	public static int countEvensG(List<Integer> L) {
+	
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> numbersA = new ArrayList<Integer>();
+		List<Integer> numbersL = new LinkedList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			numbersA.add(i);
+			numbersL.add(i);
+		}
+	}
+}
