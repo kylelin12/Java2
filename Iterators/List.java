5bb661051d6e785bd88f6fc5d8319dae09cad024
Kyle <klin12@stuy.edu>
2017-04-21 13:25:51 -0400

    Slight alteration to List.java

--- Iterators/List.java
+++ Iterators/List.java
@@ -1,10 +1,12 @@
-interface List<E>{
+import java.util.Iterator;
+
+interface List<E> extends Iterable<E>{
     int size();
     boolean add(E obj); // adds a obj to the end
     void add(int index, E obj);
     E get(int index);
     E set(int index, E obj);
     E remove(int index);
-
+    Iterator<E> iterator();
 
 }
