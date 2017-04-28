ec0db97a2221687b3c75cf2b65ce98926440d49d
Kyle <klin12@stuy.edu>
2017-04-28 13:57:51 -0400

    Initial commit of SparseArray

---
+++ AP Review/SparseArray.java
@@ -0,0 +1,28 @@
+public class SparseArray {
+	private int numRows;
+	private int numCols;
+	
+	private List<SparseArrayEntry> entries;
+	
+	public SparseArray() {
+		entries = new ArrayList<SparseArrayEntry>();
+	}
+	
+	public int getNumRows() {
+		return numRows;
+	}
+	
+	public int getNumCols() {
+		return numCols;
+	}
+	
+	// Returns the value of the element at row row and col col.
+	public int getValueAt(int row, int col) {
+		
+	}
+	
+	// Removes the col col from the Sparse Array.
+	public void removeColumn(int col) {
+	
+	}
+}
