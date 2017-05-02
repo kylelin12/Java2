d3699c5d9fab6175c00d4f4e665a0bbba482564e
Kyle <klin12@stuy.edu>
2017-05-02 14:04:08 -0400

    Implemented most methods in TreeApp

--- Binary Tree/TreeApp.java
+++ Binary Tree/TreeApp.java
@@ -56,6 +56,46 @@ public class TreeApp {
 		}
 		return count;
 	}
+	
+	// returns true if the node is a leaf false otherwise.
+	public static < E > boolean isLeaf(TreeNode < E > root) {
+		return (root.getLeft() != null & root.getRight() != null);
+	}
+	
+	// returns the number of leaves in a binary tree.
+	public static < E > int countLeaves(TreeNode< E > root) {
+		if (root == null) return 0;
+		int ans = 1;
+		if (isLeaf(root)) return ans;
+		return ans + countLeaves(root.getLeft()) + countLeaves(root.getRight());
+	}
+	
+	// returns the height of a binary tree.
+	public static < E > int height(TreeNode < E > root) {
+		if (root == null) return 0;
+		int maxL = height(root.getLeft());
+		int maxR = height(root.getRight());
+		if (maxL > maxR)
+			return maxL + 1;
+		return maxR + 1;
+	}
+	
+	
+	// inserts a new leaf into a binary 
+    // tree by placing the leaf into the left or right subtree with fewer nodes.
+    // The height of the tree should only increase if the tree was completely
+    // balanced.
+	// post condition: inserts a leaf into the tree and returns the 
+	//                 root of the tree.
+    /*public static < E > TreeNode < E > insertBalanced(TreeNode < E > root, E value) {
+    	if (root == null) return new TreeNode < E > (value, null, null);
+    	if (height(root.getLeft()) < height(root.getRight()) {
+    		
+    	} else {
+    		
+    	}
+    	return root;
+    }*/
 
 	public static void main(String[] args) {
 		TreeNode < Integer > root = new TreeNode < Integer > (1, new TreeNode < Integer > (2, null, new TreeNode < Integer > (4)), new TreeNode < Integer > (3, new TreeNode < Integer > (5, new TreeNode < Integer > (7), new TreeNode < Integer > (8)), new TreeNode < Integer > (6)));
@@ -69,6 +109,14 @@ public class TreeApp {
 
 		System.out.println("# nodes: " + countNodes(root));
 		System.out.println("# nodes: " + countNodesI(root));
+		
+		System.out.println("# leaves: " + countLeaves(root));
+		
+		/*
+		insertBalanced(root, "test")
+		System.out.println("Balanced Insert: ");
+		traversePostorder(root);
+		*/
 
 	}
 
