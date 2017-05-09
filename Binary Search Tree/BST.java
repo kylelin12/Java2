fc4533853d2f35b282b659829e7092316ceaa8d1
Kyle <klin12@stuy.edu>
2017-05-09 13:57:51 -0400

    Added remove() properly. Reformatted code

--- Binary Search Tree/BST.java
+++ Binary Search Tree/BST.java
@@ -1,9 +1,7 @@
-public class BST < E extends Comparable > {
+import java.util.Iterator;
+import java.util.ArrayList;
 
-	/* Ordering property: Given a root, the items contained
-       in the left subtree are less than the root, the items
-       in the right subtree are greater or equal to the root.
-    */
+public class BST < E extends Comparable < E >> implements Iterable < E > {
 
 	private TreeNode < E > _root;
 	private int _size;
@@ -12,6 +10,7 @@ public class BST < E extends Comparable > {
 		_root = null;
 		_size = 0;
 	}
+
 	public boolean isEmpty() {
 		return _size == 0;
 	}
@@ -20,157 +19,164 @@ public class BST < E extends Comparable > {
 		return _size;
 	}
 
-	public void insert(E x) {
-		insertNode(new TreeNode < E > (x));
+	// Iterative version
+	// returns null if val not found
+	public TreeNode < E > find(E val) {
+		TreeNode < E > curr = _root;
+		while (curr != null && !val.equals(curr.getValue())) {
+			if (val.compareTo(curr.getValue()) < 0) curr = curr.getLeft();
+			else curr = curr.getRight();
+		}
+		return curr;
+	}
+	//********************************************************
+	// Recursive version
+	// post: return null if x not found
+	public TreeNode < E > findR(E x) {
+		return findR(_root, x);
 	}
 
-	// ******* Question 1 **********************
-	// inserts the node into the BST.
-	// Uses piggybacking
+	private TreeNode < E > findR(TreeNode < E > rt, E x) {
+		if (rt == null || x.equals(rt.getValue())) return rt;
+		if (x.compareTo(rt.getValue()) < 0) return findR(rt.getLeft(), x);
+		return findR(rt.getRight(), x);
+	}
+	//********************************************************
+	public boolean isFound(E x) {
+		return find(x) != null;
+	}
+
+	//********************************************************
+	// pre: !isEmpty()
+	public E maxValue() throws IllegalStateException {
+		return maxValue(_root);
+	}
+
+	private E maxValue(TreeNode < E > rt) throws IllegalStateException {
+		if (rt == null) throw new IllegalStateException("empty");
+		return maxNode(rt).getValue();
+	}
+	//********************************************************
+	private TreeNode < E > maxNode(TreeNode < E > rt) {
+		if (rt != null) while (rt.getRight() != null) rt = rt.getRight();
+		return rt;
+	}
+	//********************************************************
+	// pre: !isEmpty()
+	public E minValue() throws IllegalStateException {
+		return minValue(_root);
+	}
+
+	private E minValue(TreeNode < E > rt) throws IllegalStateException {
+		if (rt == null) throw new IllegalStateException("empty");
+		return minNode(rt).getValue();
+	}
+	//********************************************************
+	private TreeNode < E > minNode(TreeNode < E > rt) {
+		if (rt != null) while (rt.getLeft() != null) rt = rt.getLeft();
+		return rt;
+	}
+	//********************************************************
+	/* Remove Algorithm for a BST
+       ==========================
+       Removing val from a BST.
+       Steps
+       1. if the tree is empty then stop.
+       2. if the val is a leaf remove the leaf.
+       3. if the val is an internal node with no right subtree,
+          then transfer the maximum value,max, from the left subtree
+          into the node that contains val, then remove max from the
+          left subtree.  
+       4. otherwise transfer the minimum value, min, from the right subtree
+          into the node that contains val, then remove min from the right
+           subtree.
+    */
+
+	public boolean remove(E val) {
+		if (isFound(val)) {
+			_root = remove(_root, val);
+			_size--;
+			return true;
+		}
+		return false;
+	}
+	// val is in the tree
+	private TreeNode < E > remove(TreeNode < E > rt, E val) {
+		if (isLeaf(rt)) return null;
+		if (val.equals(rt.getValue())) {
+			if (rt.getRight() == null) {
+				E max = maxValue(rt.getLeft());
+				rt.setValue(max);
+				rt.setLeft(remove(rt.getLeft(), max));
+			} else {
+				E min = minValue(rt.getRight());
+				rt.setValue(min);
+				rt.setRight(remove(rt.getRight(), min));
+			}
+		} else {
+			if (val.compareTo(rt.getValue()) < 0) rt.setLeft(remove(rt.getLeft(), val));
+			else rt.setRight(remove(rt.getRight(), val));
+		}
+		return rt;
+	}
+	private boolean isLeaf(TreeNode < E > rt) {
+		return (rt.getLeft() == null && rt.getRight() == null);
+	}
+	//********************************************************
+	// Recursive version
+	// inserts x as a  leaf
+	public void insert(E x) {
+		_root = insertNode(_root, new TreeNode < E > (x));
+		_size++;
+	}
+
+	private TreeNode < E > insertNode(TreeNode < E > rt, TreeNode < E > leaf) {
+		if (rt == null) return leaf;
+		E val = leaf.getValue();
+		if (val.compareTo(rt.getValue()) < 0) rt.setLeft(insertNode(rt.getLeft(), leaf));
+		else rt.setRight(insertNode(rt.getRight(), leaf));
+		return rt;
+	}
+
+	//********************************************************
+	// Iterative version
+	// inserts a leaf
 	private void insertNode(TreeNode < E > leaf) {
 		E val = leaf.getValue();
 		if (_root == null) _root = leaf;
 		else {
-			TreeNode < E > prev = _root;
 			TreeNode < E > curr = _root;
+			TreeNode < E > prev = curr;
 			while (curr != null) {
 				prev = curr;
-				if (val.compareTo(curr.getValue()) < 0)
-					curr = curr.getLeft();
-				else
-					curr = curr.getRight();
+				if (val.compareTo(curr.getValue()) >= 0) curr = curr.getRight();
+				else curr = curr.getLeft();
 			}
-			if (val.compareTo(prev.getValue()) < 0)
-				prev.setLeft(leaf);
-			else
-				prev.setRight(leaf);
+			if (val.compareTo(prev.getValue()) >= 0) prev.setRight(leaf);
+			else prev.setLeft(leaf);
+
 		}
 		_size++;
 	}
-	
-	// Question 2
-	// Recursive version of insertion
-	public void insertR(E val) {
-		_root = insertNode(_root, new TreeNode < E >(val));
-		_size++;
-	}
-
-	public TreeNode < E > insertNode(TreeNode < E > rt, TreeNode < E > leaf) {
-		if (rt == null) return leaf;
-		E val = leaf.getValue();
-		if (val.compareTo(rt.getValue()) < 0)
-			rt.setLeft(insertNode(rt.getLeft(), leaf));
-		else
-			rt.setRight(insertNode(rt.getRight(), leaf));
-		return rt;
-	}
-	
-	// Question 3
-	// O(LogN) find()
-	// Finds a value and reports that node
-	public TreeNode < E > find(E val) {
-		TreeNode < E > curr = _root;
-		while (curr != null) {
-			if (val == curr.getValue())
-				return curr;
-			else if (val.compareTo(curr.getValue()) < 0)
-				curr = curr.getLeft();
-			else
-				curr = curr.getRight();
+	//********************************************************
+	public String toString() {
+		String ans = toString(_root);
+		if (ans.length() > 1) {
+			int pos = ans.lastIndexOf(",");
+			ans = ans.substring(0, pos);
 		}
-		return null;
+		return ans;
 	}
-
-	public TreeNode < E > findR(E val) {
-		return find(_root, val);
-	}
-
-	private TreeNode < E > find(TreeNode < E > rt, E val) {
-		if (rt == null) return null;
-		if (val.compareTo(rt.getValue()) < 0)
-			return find(rt.getLeft(), val);
-		else if (val.compareTo(rt.getValue()) > 0)
-			return find(rt.getRight(), val);
-		return rt;
-	}
-	
-	// Problem 4
-	// Write the BST method that returns the max value
-	private TreeNode < E > maxNode(TreeNode < E > rt) {
-		if (rt == null) return null;
-		while (rt.getRight() != null)
-			rt = rt.getRight();
-		return rt;
-	}
-	
-	private TreeNode < E > maxNodeR(TreeNode < E > rt) {
-		if (rt == null) return null;
-		if (rt.getRight() == null)
-			return rt;
-		return maxNodeR(rt.getRight());
-	}
-	
-	// Problem 5
-	// Write the BST method that removes the node with that value
-	
-	public boolean isFound(E x) {
-		return find(x) != null;
-	}
-	
-	public E maxValue() throws IllegalStateException {
-		return maxValue(_root);
-	}
-	
-	private E maxValue(TreeNode < E > rt) throws IllegalStateException {
-		if (rt == null) throw new IllegalStateException("Empty");
-		return maxNode(rt).getValue();
-	}
-	
-	private TreeNode < E > maxNode(TreeNode < E > rt) {
+	// Uses an inorder traversal.
+	private String toString(TreeNode < E > rt) {
+		String ans = "";
 		if (rt != null) {
-			while (rt.getRight() != null)
-			rt = rt.getright();
+			ans = toString(rt.getLeft()) + rt.getValue() + ", " + toString(rt.getRight());
 		}
-		return rt;
+		return ans;
 	}
-	
-	public E minValue() throws IllegalStateException {
-		return minValue(_root);
-    }
-
-    private E minValue(TreeNode< E > rt) throws IllegalStateException {
-		if (rt == null) throw new IllegalStateException("empty");	
-		return minNode(rt).getValue();
-    }
-    
-    private TreeNode< E > minNode(TreeNode< E > rt) {
-		if (rt != null) 
-	 	   while (rt.getLeft() != null) 
-			rt = rt.getLeft();
-		return rt;
-    }
-
-	public boolean remove(E val) {
-		if (isFound(val)){
-		    _root = remove(_root, val);
-		    _size--;
-		    return true;
-		}
-		return false;
-    }
-
-	// pre: val is in the tree
-    // post : removes one occurrence of val in the tree,
-    //        returns the root of the tree (subtree) that
-    //        contained the removed val.
-    private TreeNode< E > remove(TreeNode< E > rt, E val) {
-		if (rt == null) return null;
-		if (rt.getLeft() == null && rt.getRight() == null) return null;
-		if (rt.getRight() == null) {
-			
-		}
-    }
-
+	//********************************************************
+	//********************************************************
 	public void inorder() {
 		System.out.print("inorder: ");
 		inorderTraversal(_root);
@@ -183,22 +189,127 @@ public class BST < E extends Comparable > {
 		System.out.print(rt.getValue() + " ");
 		inorderTraversal(rt.getRight());
 	}
+	//********************************************************
+	public void preorder() {
+		System.out.print("preorder: ");
+		preorderTraversal(_root);
+		System.out.println();
+	}
 
+	private void preorderTraversal(TreeNode < E > rt) {
+		if (rt == null) return;
+		System.out.print(rt.getValue() + " ");
+		preorderTraversal(rt.getLeft());
+		preorderTraversal(rt.getRight());
+	}
+	//********************************************************
+	public void postorder() {
+		System.out.print("postorder: ");
+		postorderTraversal(_root);
+		System.out.println();
+	}
+
+	private void postorderTraversal(TreeNode < E > rt) {
+		if (rt == null) return;
+		postorderTraversal(rt.getLeft());
+		postorderTraversal(rt.getRight());
+		System.out.print(rt.getValue() + " ");
+	}
+	// ***************** Question 5 ****************************
+	/*  Add an iterator for the BST.
+        The iterator must process the data inorder.
+        A call to hasNext() and next() must be O(1).
+        A call to remove() must be O(log N).
+     */
+	// *********************************************************
+	// *********************************************************
+	// needed for the Iterable interface
+	public Iterator < E > iterator() {
+		return null;
+	}
+
+	private class BST_Iterator implements Iterator < E > {
+		private BST < E > _tree;
+		private ArrayList < E > _list;
+		// a list of the BST entries
+		// stored in increasing order 
+		private Iterator < E > _itr;
+		private E _valReturned;
+		// last value returned by a call of next();
+		// will be null if either next() was not called or when
+		//              remove() is called 
+		// O(N log N)
+		// populates a list of the tree's entries in increasing order
+		public BST_Iterator(BST < E > tree) {
+
+}
+		// O(N)
+		// populates L with the vales from the BST represented by rt.
+		// L is in increasing order.
+		private void populate(ArrayList < E > L, TreeNode < E > rt) {
+			return;
+		}
+
+		// O(1)
+		// checks if _list's iterator has a next value.
+		public boolean hasNext() {
+			return true;
+		}
+
+		// O(1)
+		// Initializes _valReturned and returns the
+		// value of _valReturned.
+		public E next() throws IllegalStateException {
+			return null;
+		}
+
+		// O(logN)
+		// removes the value from the BST not the list
+		// sets the _valReturned variable to null
+		public void remove() throws IllegalStateException {
+			return;
+		}
+	}
+	// *********** End of Question 5 ************************
+	// ******************************************************
+	//********************************************************
 	public static void main(String[] args) {
 		BST < Integer > tree = new BST < Integer > ();
-		int N = Integer.parseInt(args[0]);
+		int N = 10; //Integer.parseInt(args[0]);
 		for (int i = 0; i < N; i++) {
-			int r = (int)(Math.random() * 5);
+			int r = (int)(Math.random() * 100);
 			System.out.println("insert " + r);
-			tree.insertR(r);
+			tree.insert(r);
+			System.out.println("tree: " + tree);
 		}
+
 		tree.inorder();
-		
-		System.out.println("Finding Iterative (4): " + tree.find(4));
-		System.out.println("Finding Recursive (3): " + tree.findR(3));
-		
-		System.out.println("MaxNode Iterative: " + tree.maxNode(tree._root));
-		System.out.println("MaxNode Recursive: " + tree.maxNodeR(tree._root));
+		tree.preorder();
+		tree.postorder();
+		System.out.println("Max value: " + tree.maxValue());
+
+		while (!tree.isEmpty()) {
+			Integer max = tree.maxValue();
+			System.out.println("max : " + max);
+			tree.remove(max);
+			System.out.println("remove max: " + tree);
+		}
+
+		/*
+	for(Integer n : tree){
+	    System.out.println(n);
+	}
+
+	Iterator<Integer> itr = tree.iterator();
+	while (itr.hasNext()){
+	    if (itr.next() % 2 == 0) itr.remove();
+	    //itr.remove(); // should throw an exception
+	}
+
+	for(Integer n : tree){
+	    System.out.println(n);
+	}
+	*/
 	}
 
 }
