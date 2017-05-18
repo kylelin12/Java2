d827e12ddf7bca1c6ed78c04f7b3611941635b2d
Kyle <klin12@stuy.edu>
2017-05-18 13:41:42 -0400

    Fixed removeMinChild and added + implemented the minChildPos method

--- Priority Queue and heaps/Demo_II.java
+++ Priority Queue and heaps/Demo_II.java
@@ -17,16 +17,33 @@ public class Demo_II {
 		}
 	}
 
-	public static void removeMin(ArrayList < Integer > heap) {
-		heap.set(0, heap.set(heap.size() - 1, heap.get(0)));
-		heap.remove(heap.size() - 1);
-		if (heap.getSize() > 0) removeHelper(0, heap);
+	public static int removeMin(ArrayList < Integer > heap) {
+		int ans = heap.get(0);
+		int x = heap.remove(heap.size() - 1);
+		if (heap.size() > 0) {
+			heap.set(0, x);
+			int pos = 0;
+			int mcPos = minChildPos(pos, heap);
+			while (mcPos != -1) {
+				if (heap.get(pos) < heap.get(mcPos))
+					break;
+				heap.set(pos, heap.set(mcPos, heap.get(pos)));
+				pos = mcPos;
+				mcPos = minChildPos(pos, heap);
+			}
+		}
+		return ans;
 	}
 	
-	public static void removeHelper(int index, ArrayList < Integer > heap) {
-		int childL = (index * 2 + 1);
-		int childR = (childL + 1);
-		while ((heap.get(childL) != null && heap.get(childR) != null) || heap.get(index)
+	
+	// pre: heap != null
+	// post: returns the position of the minimum child of value at pos or -1 if pos is a leaf
+	public static int minChildPos(int pos, ArrayList < Integer > heap) {
+		int childL = pos * 2 + 1;
+		int childR = childL + 1;
+		if (childL >= heap.size()) return -1;
+		if (right >= heap.size() || heap.get(left) <= heap.get(right)) return left;
+		return right;
 	}
 
 	public static void main(String[] args) {
